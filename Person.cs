211728451e0179350b190b6fb283c56e53cf954d
swetha969 <swetha969@users.noreply.github.com>
2017-12-07 12:30:53 -0500

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxAlive
+{
+    public class Person
+    {
+        public int YearBorn; 
+        public int YearDied; 
+       
+
+        public List<Person> GetPersons()
+        {
+            List<Person> persons = new List<Person>
+            {
+                new Person { YearBorn=1905,YearDied=1965 },
+                new Person { YearBorn=1985,YearDied=1995 },
+                new Person { YearBorn=1910,YearDied=1975 },
+                new Person { YearBorn=1915,YearDied=1998 },
+                new Person { YearBorn=1987,YearDied=1999 },
+                new Person { YearBorn=1906,YearDied=1988 },
+                new Person { YearBorn=1903,YearDied=1956 },
+                new Person { YearBorn=1926,YearDied=1999 },
+                new Person { YearBorn=1919,YearDied=1987 },
+                new Person { YearBorn=1947,YearDied=1986 },
+                new Person { YearBorn=1965,YearDied=1995 },
+                new Person { YearBorn=1966,YearDied=1997 },
+                new Person { YearBorn=1909,YearDied=1928 },
+                new Person { YearBorn=1904,YearDied=1965 },
+                new Person { YearBorn=1954,YearDied=1998 },
+            };
+            return persons;
+        }
+
+
+        //Other properties, methods, events...
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MaxAlive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            List<Person> lstPrsn = person.GetPersons();
+            List<int> aliveYrs = new List<int>();
+
+            foreach(Person prsn in lstPrsn)
+            {
+                if(prsn.YearBorn < prsn.YearDied && prsn.YearBorn>=1900 && prsn.YearDied<=2000)
+                {
+                    for(int i= prsn.YearBorn; i<= prsn.YearDied;i++)
+                    {
+                       aliveYrs.Add(i);
+                    }
+                }
+            }
+
+            var counts = aliveYrs
+                        .GroupBy(item => item)
+                        .Select(grp => new { Number = grp.Key, Count = grp.Count() });
+
+            int maxCount = counts.Max(s => s.Count);
+
+            List<int> maxAliveYrs = aliveYrs.GroupBy(x => x).Where(x => x.Count() == maxCount).Select(x => x.Key).ToList<int>();
+
+            string strMostAliveYrs = string.Empty;
+
+            if (maxAliveYrs.Count > 0)
+            {
+                foreach (int yr in maxAliveYrs)
+                {
+                    if (strMostAliveYrs.IndexOf(yr.ToString()) < 0)
+                    {
+                        if (strMostAliveYrs == string.Empty)
+                            strMostAliveYrs = yr.ToString();
+                        else
+                            strMostAliveYrs = strMostAliveYrs + "," + yr.ToString();
+                    }
+                }
+
+                if (maxAliveYrs.Count == 1)
+                {
+                    Console.WriteLine("Max people are alive in year : " + strMostAliveYrs);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Max people are alive in years : " + strMostAliveYrs);
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Date Issue");
+                Console.ReadLine();
+            }
+        }
+    }
+}
